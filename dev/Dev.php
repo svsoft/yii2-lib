380357679cf074a8acf8128cb1e4ce7ad6c412e8
Viktor <shiryakovv@gmail.com>
2017-04-10 12:46:41 +0700

    added demo class Dev

---
+++ dev/Dev.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: viktor
+ * Date: 09.04.2017
+ * Time: 23:33
+ */
+
+namespace svsoft\yii\dev;
+
+class Dev
+{
+    public static function dev()
+    {
+        echo 'dev';
+    }
+}
+
